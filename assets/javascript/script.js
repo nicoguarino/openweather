8a7b5bd77d7fe6e76d937318846427e55ed00be0
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-09-08 17:28:54 -0700

    gave an id to h2 element in html, refactored and revised getWeather Func, able to save local storage to array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,22 +2,24 @@ var userSearchEl = document.querySelector("#city-search");
 var searchBarEl = document.querySelector("#search-bar");
 var cityTitleEl = document.querySelector('#forecast-city');
 
+// checking to see if there is any exsiting data in local storage and if not assigning a blank array
+var searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
+
+
 
 // Submit City Function
-var CitySubmitHandler = function(event) {
+var CitySubmitHandler = function (event) {
     event.preventDefault();
 
-    var InputEl = userSearchEl.value.trim(); 
+    var InputEl = userSearchEl.value.trim();
 
     if (InputEl) {
-       
+
         saveCityEl(InputEl);
-        
+
         getWeather(InputEl);
 
-
-
-      // userSearchEl.value = "";
+        // userSearchEl.value = "";
 
     } else {
         alert("City searched is not found.");
@@ -33,49 +35,60 @@ var CitySubmitHandler = function(event) {
 // needs to use different api. This one does have uv or future forcast
 var getWeather = function (search) {
     // format the api url
-    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" 
-    + search +
-    "&appid=d3f5af43f561d831f34569cf6fef321f";
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q="
+        + search +
+        "&appid=d3f5af43f561d831f34569cf6fef321f";
 
     fetch(weatherApi)
-        .then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    console.log(data);
-                    displayWeather(data, data.main.name);
+        .then(function (response) {
+           return response.json();
+
+        })
+        .then(function (data) {
+
+            // displaying city Searched
+            cityTitleEl.textContent = data.name;
+
+            //setting city searched to local storage
+            localStorage.setItem('searchedCityEl', JSON.stringify(data.name));
+
+            // pushing name to array in local storage
+            searchedCities.push(data.name);
+            console.log(searchedCities);
+
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat="
+                + data.coord.lat + "&lon=" + data.coord.lon
+                + "&appid=d3f5af43f561d831f34569cf6fef321f")
+                .then(function (response) {
+                    return response.json();
                 })
-            } else {
-                alert("City not found.")
-            } 
+                .then(function(data){
+                    displayWeather(data);
+                });
+
         });
+
 };
 
 // trying to pull api content and display
-var displayWeather = function(weatherData, cityNameEl) {
+var displayWeather = function (weatherData) {
 
-    cityTitleEl.textcontent = cityNameEl;
-
-    if (weatherData === 0) {
-
-        cityTitleEl.textcontent = "City Not Found."
-        return;
-    }
+    console.log(weatherData);
 
 
 };
 
-// can't get inputEl to save to storageArray
+// can't get inputEl to save to searchedCities array
 var saveCityEl = function (InputEl) {
-    var storageArray = [InputEl];
-    window.localStorage.setItem('searchedCityEl', JSON.stringify(storageArray.value));
+  //  localStorage.setItem('searchedCityEl', JSON.stringify(searchedCities.value));
 
-    storageArray = JSON.parse(window.localStorage.getItem('savedCity'));
+    //searchedCities = JSON.parse(localStorage.getItem('searchCities'));
     // $('.search-history').html('');
 
-    for (let i = 0; i <= storageArray.length; i++) {
-        $('.search-history').append("<button>" + storageArray[i] + "</button>");
-        
-        return storageArray[i];
+    for (let i = 0; i <= searchedCities.length; i++) {
+        $('.search-history').append("<button>" + searchedCities[i] + "</button>");
+
+        return searchedCities[i];
     }
 };
 
